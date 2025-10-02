8fa93b8ba55ece09375644981ac4bd0f44f82af6
Richard Baah <richardbaah@MacBook-Air-de-Richard.local>
2025-10-02 11:47:18 -0400

    Implemented HashTable Manager for fields. Next step implement a linked list backed List manager

--- src/memoryStore/cache.go
+++ src/memoryStore/cache.go
@@ -2,6 +2,7 @@ package memorystore
 
 import (
 	"RapidStore/memoryStore/internal"
+	"RapidStore/memoryStore/internal/DS"
 	"errors"
 	"fmt"
 	"reflect"
@@ -14,7 +15,7 @@ var (
 
 type basicSet struct {
 	key   string
-	value generalValue
+	value GeneralValue
 }
 
 type LimitedStorage interface {
@@ -39,21 +40,22 @@ type KeyInterface interface {
 }
 
 type HashTableManager interface {
-	HSet(key, field string, value any) bool // set a field ex: HSET user:!23 name "alice"
-	HGet(key, field string) (any, bool)     // get a field ex: HGET user:!23 name -> "alice"
-	HGetAll(key string) map[string]any      // HGETALL user:!23 -> map[name:alice age:30]
-	HDel(key, field string) bool
+	HSet(key, field string, value any, duration time.Duration) bool // set a field ex: HSET user:!23 name "alice" (bool indicates wheater a new hashset was created)
+	HGet(key, field string) (any, error)                            // get a field ex: HGET user:!23 name -> "alice"
+	HGetAll(key string) map[string]any                              // HGETALL user:!23 -> map[name:alice age:30]
+	HDel(key, field string)
 	HExists(key, field string) bool
 	LimitedStorage
 }
 
-// List (ordered)
+// List (ordered) no ttl can be provided
 type ListManager interface {
 	LPush(key string, value any) int // push start
 	RPush(key string, value any) int // push end
 	LPop(key string) (any, bool)     // pop left
 	RPop(key string) (any, bool)     // pop right
 	LRange(key string, start, stop int) []any
+	Size() uint
 	LimitedStorage
 }
 
@@ -102,16 +104,16 @@ func NewCache() Cache {
 
 /* Implements the KeyInterface */
 type keyStore struct {
-	internalData map[string]generalValue
+	internalData map[string]GeneralValue
 	length       uint64
 	policy       internal.EvictionPolicy
 }
-type generalValue struct {
-	value any
+type GeneralValue struct {
+	Value any
 	TTL   time.Time
 }
 
-func (k *keyStore) validKey(key string, valuePair generalValue) bool {
+func (k *keyStore) validKey(key string, valuePair GeneralValue) bool {
 	// If TTL is set to neverExpires, the key is always valid
 	if valuePair.TTL.Equal(neverExpires) {
 		return true
@@ -124,8 +126,8 @@ func (k *keyStore) validKey(key string, valuePair generalValue) bool {
 	return true
 }
 func (k *keyStore) SetKey(key string, value any) {
-	v := generalValue{
-		value: value,
+	v := GeneralValue{
+		Value: value,
 		TTL:   neverExpires,
 	}
 	k.internalData[key] = v
@@ -136,7 +138,7 @@ func (k *keyStore) GetKey(key string) any {
 	if !ok || !k.validKey(key, v) {
 		return ""
 	}
-	return v.value
+	return v.Value
 }
 func (k *keyStore) DeleteKey(key string) {
 	delete(k.internalData, key)
@@ -172,12 +174,12 @@ func (k *keyStore) Increment(key string) (int64, error) {
 	v, exists := k.internalData[key]
 	if !exists || !k.validKey(key, v) {
 		// Initialize with 1 if key doesn't exist or is expired
-		newVal := generalValue{value: int64(1), TTL: neverExpires}
+		newVal := GeneralValue{Value: int64(1), TTL: neverExpires}
 		k.internalData[key] = newVal
 		return 1, nil
 	}
 
-	val := reflect.ValueOf(v.value)
+	val := reflect.ValueOf(v.Value)
 	if !val.Type().ConvertibleTo(reflect.TypeOf(int64(0))) {
 		return 0, errors.New("value is not a numeric type")
 	}
@@ -185,7 +187,7 @@ func (k *keyStore) Increment(key string) (int64, error) {
 	// Convert to int64, increment, and store back
 	numVal := val.Convert(reflect.TypeOf(int64(0))).Int()
 	numVal++
-	v.value = numVal
+	v.Value = numVal
 	k.internalData[key] = v
 
 	return numVal, nil
@@ -194,12 +196,12 @@ func (k *keyStore) Decrement(key string) (int64, error) {
 	v, exists := k.internalData[key]
 	if !exists || !k.validKey(key, v) {
 		// Initialize with -1 if key doesn't exist or is expired
-		newVal := generalValue{value: int64(-1), TTL: neverExpires}
+		newVal := GeneralValue{Value: int64(-1), TTL: neverExpires}
 		k.internalData[key] = newVal
 		return -1, nil
 	}
 
-	val := reflect.ValueOf(v.value)
+	val := reflect.ValueOf(v.Value)
 	if !val.Type().ConvertibleTo(reflect.TypeOf(int64(0))) {
 		return 0, fmt.Errorf("invalid value type for key %s", key) // or handle error appropriately
 	}
@@ -207,7 +209,7 @@ func (k *keyStore) Decrement(key string) (int64, error) {
 	// Convert to int64, decrement, and store back
 	numVal := val.Convert(reflect.TypeOf(int64(0))).Int()
 	numVal--
-	v.value = numVal
+	v.Value = numVal
 	k.internalData[key] = v
 
 	return numVal, nil
@@ -216,18 +218,18 @@ func (k *keyStore) Append(key string, suffix string) error {
 	v, exists := k.internalData[key]
 	if !exists || !k.validKey(key, v) {
 		// If key doesn't exist or is expired, set it to the new value
-		newVal := generalValue{value: suffix, TTL: neverExpires}
+		newVal := GeneralValue{Value: suffix, TTL: neverExpires}
 		k.internalData[key] = newVal
 		return nil
 	}
 
-	strVal, ok := v.value.(string)
+	strVal, ok := v.Value.(string)
 	if !ok {
 		return errors.New("key is of invalid type") // or handle error appropriately
 	}
 
 	strVal += suffix
-	v.value = strVal
+	v.Value = strVal
 	k.internalData[key] = v
 
 	return nil
@@ -264,6 +266,8 @@ func (k *keyStore) Evict() {
 		// Unknown policy, handle error or default behavior
 	}
 }
+
+// TODO: implement eviction policies
 func (k *keyStore) Policy(policy internal.EvictionPolicy) {}
 func (k *keyStore) Keys() []string { // list all keys in the store
 	var res []string
@@ -276,11 +280,156 @@ func (k *keyStore) Keys() []string { // list all keys in the store
 }
 func NewKeyStore(maxSize uint64, policy string) KeyInterface {
 	return &keyStore{
-		internalData: make(map[string]generalValue),
+		internalData: make(map[string]GeneralValue),
 		length:       maxSize,
 		policy:       internal.EvictionPolicy(topolicy(policy)),
 	}
 }
+
+type FieldStore struct {
+	FieldData map[string]map[string]GeneralValue //key : {user:(value,ttl)} , name: alice , age: 30
+	length    uint64
+	policy    internal.EvictionPolicy
+}
+
+func (f *FieldStore) validKey(key string, field string, valuePair GeneralValue) bool {
+	// If TTL is set to neverExpires, the key is always valid
+	if valuePair.TTL.Equal(neverExpires) {
+		return true
+	}
+	// Check if the key has expired
+	if time.Now().After(valuePair.TTL) {
+		delete(f.FieldData[key], field)
+		return false
+	}
+	return true
+}
+
+func (f *FieldStore) HSet(key, field string, value any, TTL time.Duration) bool {
+	v, ok := f.FieldData[key]
+	if !ok {
+		// if it doesnt exist allocate new map and set the field to the value
+		f.FieldData[key] = make(map[string]GeneralValue)
+		f.FieldData[key][field] = GeneralValue{Value: value, TTL: time.Now().Add(TTL)}
+		return false
+	}
+	/// if it exist use the existing hashtable and assign the field to the value
+	v[field] = GeneralValue{Value: value, TTL: time.Now().Add(TTL)}
+	return true
+}
+
+func (f *FieldStore) HGet(key, field string) (any, error) { // get a field
+	v, ok := f.FieldData[key]
+	if !ok {
+		return nil, errors.New("key doesnt exist")
+	}
+	x, exist := v[field]
+	if !exist || !f.validKey(key, field, x) {
+		return nil, fmt.Errorf("field %s of key %s doesnt exist", field, key)
+	}
+	return x, nil
+
+}
+
+func (f *FieldStore) HGetAll(key string) map[string]any { // HGETALL
+	var returnValues = make(map[string]any)
+	for k, v := range f.FieldData[key] {
+		if !f.validKey(key, k, v) {
+			continue
+		}
+		returnValues[k] = v
+	}
+	return returnValues
+}
+
+func (f *FieldStore) HDel(key, field string) {
+	v, ok := f.FieldData[key]
+	if !ok || !f.validKey(key, field, v[field]) {
+		fmt.Printf("(HDEL):: key %s doesnt exist\n", key)
+		return
+	}
+	delete(v, field)
+}
+func (f *FieldStore) HExists(key, field string) bool {
+	v, ok := f.FieldData[key]
+	if !ok || !f.validKey(key, field, v[field]) {
+		return false
+	}
+	_, ok = v[field]
+	return ok
+
+}
+func (f *FieldStore) CurrentSize() uint64 {
+	return uint64(len(f.FieldData))
+}
+func (f *FieldStore) Evict() {
+
+	switch f.policy {
+	case internal.NoEviction:
+		// Do nothing, new values aren't saved when memory limit is reached
+	case internal.LRU:
+		// Implement LRU eviction logic here
+	case internal.LFU:
+		// Implement LFU eviction logic here
+	case internal.RANDOM:
+		// Implement RANDOM eviction logic here
+	case internal.VolatileLRU:
+		// Implement Volatile LRU eviction logic here
+	case internal.VolatileLFU:
+		// Implement Volatile LFU eviction logic here
+	case internal.VolatileRandom:
+		// Implement Volatile RANDOM eviction logic here
+	case internal.VolatileTTL:
+		// Implement Volatile TTL eviction logic here
+	default:
+		panic("unknown eviction policy")
+		// Unknown policy, handle error or default behavior
+	}
+}
+func (f *FieldStore) Keys() []string { // list all keys in the store
+	var res []string
+	for key := range f.FieldData {
+		if !f.validKey(key, "", GeneralValue{TTL: neverExpires}) {
+			continue
+		}
+		res = append(res, key)
+	}
+	return res
+}
+
+// NewFieldStore creates a new instance of FieldStore with the specified maximum size and eviction policy.
+func NewFieldStore(maxSize uint64, policy string) HashTableManager {
+	return &FieldStore{
+		FieldData: make(map[string]map[string]GeneralValue),
+		length:    maxSize,
+		policy:    internal.EvictionPolicy(topolicy(policy)),
+	}
+}
+
+type OrderedListStore struct {
+	internalManager DS.SequenceStorage
+}
+
+func NewOrderedListStore() *OrderedListStore {
+	return &OrderedListStore{
+		internalManager: DS.NewSequenceStorage(),
+	}
+}
+
+func (o *OrderedListStore) LPush(key string, value any) int
+func (o *OrderedListStore) RPush(key string, value any) int
+func (o *OrderedListStore) LPop(key string) (any, bool)
+func (o *OrderedListStore) RPop(key string) (any, bool)
+func (o *OrderedListStore) LRange(key string, start, stop int) []any
+func (o *OrderedListStore) Size() uint
+func (o *OrderedListStore) CurrentSize() uint64
+func (o *OrderedListStore) Evict()
+func (o *OrderedListStore) Keys() []string
+
+func NewListManager() ListManager {
+	return NewOrderedListStore()
+}
+
 func topolicy(s string) int {
 	switch s {
 	case "no eviction":
--- src/memoryStore/internal/DS/LinkedList.go
+++ src/memoryStore/internal/DS/LinkedList.go
@@ -1,3 +1,57 @@
-package internal
+package DS
 
-type LinkedList interface{}
+// Array/Linked List/ Queue
+type SequenceStorage interface {
+	AddFirst(*Node) // add @ head
+	AddLast(*Node)  // add @ tail
+	PopHead() *Node // remove head and return it
+	PopTail() *Node // remove tail and return it
+	Peak() Node     // copie of head
+	PeakTail() Node // copie of tail
+	Size() uint
+}
+type Node struct {
+	value interface{}
+	next  *Node
+	prev  *Node
+}
+
+func NewNode(value any, pointers ...*Node) *Node {
+	var next *Node = nil
+	var prev *Node = nil
+	if len(pointers) > 0 {
+		next = pointers[0]
+	}
+	if len(pointers) > 1 {
+		prev = pointers[1]
+	}
+	return &Node{
+		value: value,
+		next:  next,
+		prev:  prev,
+	}
+}
+
+type LazyList struct {
+	head *Node
+	tail *Node
+}
+
+func NewLazyList() *LazyList {
+
+	return &LazyList{
+		head: nil,
+		tail: nil,
+	}
+}
+func (L *LazyList) AddFirst(*Node) {}
+func (L *LazyList) AddLast(*Node)  {}
+func (L *LazyList) PopHead() *Node { return nil }
+func (L *LazyList) PopTail() *Node { return nil }
+func (L *LazyList) Peak() Node     { return Node{} }
+func (L *LazyList) PeakTail() Node { return Node{} }
+func (L *LazyList) Size() uint     { return 0 }
+
+func NewSequenceStorage() SequenceStorage {
+	return NewLazyList()
+}
